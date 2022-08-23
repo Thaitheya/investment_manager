ea6f935cb06b94af6eb01bcd5b91ae4080f81b82
Thaitheya <sudan25092007@gmail.com>
2022-08-23 15:21:08 +0530

    sonar issues

--- src/main/webapp/WEB-INF/views/asserts/app.js
+++ src/main/webapp/WEB-INF/views/asserts/app.js
@@ -1,10 +1,10 @@
 /**
  * 
  */
- var container  = document.getElementById('container');
+let container  = document.getElementById('container');
 window.onmousemove =  function(e) {
-	var x = - e.clientX/5;
-	    y = - e.clienty/5;
+	let x = - e.clientX/5;
+	let y = - e.clienty/5;
 	container.style.backgroundPositionX = x + 'px';
 	container.style.backgroundPositionY = y + 'py';
 
